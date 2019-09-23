d61aaf1caf00b19621b4c329614ae1bcc22410e0
machenxiang <1243858461@qq.com>
2019-09-23 08:44:55 +0800

    first commit

---
+++ src/ardupilot_command.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/HomePosition.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/WaypointList.h>
+#include <sensor_msgs/LaserScan.h>
+#include <vector>
+#include <GeographicLib/Geocentric.hpp>
+#include <eigen_conversions/eigen_msg.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <mavros/frame_tf.h>
+#include <std_msgs/Float64.h>
+#include <mavros_msgs/PositionTarget.h>
+#include<iostream>
+using namespace std;
+//自定义command消息
+#include<ardupilot_command/command.h>
+using namespace ardupilot_command;
+
+command command_now;
+int main()
+{
+    cout<<"hah";
+    return 0;
+}
